7f1fce9c8e1968a706f726ce392f18e94a5ebc5f
bsl <36967751+bosenli@users.noreply.github.com>
2022-01-30 12:12:45 -0500

    Create substring.py

---
+++ substring.py
@@ -0,0 +1,18 @@
+# Longest Substring Without Repeating Characters
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        # mp stores the current index of a character
+        mp = {}
+
+        i = 0
+        # try to extend the range [i, j]
+        for j in range(n):  #j 是自动驱动
+            if s[j] in mp: 
+                i = max(mp[s[j]], i) #如果有重复的字母，i就直接skip到除了这个字母的下一个index
+
+            ans = max(ans, j - i + 1)  #积累最长的substring， j-i + 1 ， +1是因为起点为0
+            mp[s[j]] = j + 1  #给字母map 标记要跳的index
+
+        return ans
